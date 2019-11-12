4fa9bd0872cadb00a6db3360ff79876d4598bde2
Dzmitry Kudrashou <dmitriy1983@gmail.com>
2019-11-11 20:14:35 -0600

    Create a new project

---
+++ R-P-chalange/R-P-chalange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  R-P-chalange
+//
+//  Created by Dzmitry Kudrashou on 2019-11-11.
+//  Copyright © 2019 Dzmitry Kudrashou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
